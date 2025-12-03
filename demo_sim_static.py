a932300547d0704e8f5d9f993ddf207d63052682
zhaohaojie1998 <zhaohaojie1998@qq.com>
2025-12-03 19:34:29 +0800

    路径搜索demo

--- demo_sim_static.py
+++ demo_sim_static.py
@@ -9,8 +9,11 @@
 #
 
 # 1.环境实例化
-from path_plan_env import StaticPathPlanning, NormalizedActionsWrapper
-env = NormalizedActionsWrapper(StaticPathPlanning())
+from path_plan_env import NormalizedActionsWrapper # regist to gym first
+import gymnasium as gym
+
+env = gym.make("PathSearch-v0", max_search_steps=50, render_mode="human")
+env = NormalizedActionsWrapper(env)
 
 
 # 2.策略加载
@@ -24,20 +27,20 @@ from copy import deepcopy
 MAX_EPISODE = 20
 for episode in range(MAX_EPISODE):
     ## 获取初始观测
-    obs = env.reset()
+    time_steps = 0
+    obs, _ = env.reset()
     ## 进行一回合仿真
-    for steps in range(env.max_episode_steps):
-        # 可视化
-        env.render()
+    while True:
         # 决策
         obs = obs.reshape(1, *obs.shape)                      # (*shape, ) -> (1, *shape, )
         act = policy.run(['action'], {'observation': obs})[0] # return [action, ...]
         act = act.flatten()                                   # (1, dim, ) -> (dim, )
         # 仿真
-        next_obs, _, _, info = env.step(act)
+        time_steps += 1
+        next_obs, _, done, timeout, info = env.step(act)
         # 回合结束
-        if info["terminal"]:
-            print('回合: ', episode,'| 状态: ', info,'| 步数: ', steps) 
+        if done or timeout:
+            print('回合: ', episode,'| 状态: ', info,'| 步数: ', time_steps) 
             break
         else:
             obs = deepcopy(next_obs)
